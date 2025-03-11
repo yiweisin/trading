bdd7664acba5fc2c691b680d546b831ea3f55d83
yiwei <88491304+yiweisin@users.noreply.github.com>
2025-03-11 14:43:39 +0800

    simple version

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -4,20 +4,8 @@ export default function Footer() {
       <div className="flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0">
           <p className="text-sm text-gray-500">
-            &copy; {new Date().getFullYear()} Finance Cloud. All rights
-            reserved.
+            &copy; {new Date().getFullYear()} DDAC Group 7 Assignment
           </p>
-        </div>
-        <div className="flex space-x-6">
-          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
-            Terms of Service
-          </a>
-          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
-            Privacy Policy
-          </a>
-          <a href="#" className="text-sm text-gray-500 hover:text-gray-700">
-            Contact Us
-          </a>
         </div>
       </div>
     </footer>
